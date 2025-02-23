f60d1cf523775a1fc34b6b809828ec580b650a6e
ManosFps1pt <manos.dragasakis@gmail.com>
2025-02-23 23:06:02 +0200

    changes

--- app.py
+++ app.py
@@ -9,10 +9,23 @@ NEXT_RACE = {
 	"date": "2025-03-14",  # Check this format
 }
 
+counter_time = "2025-02-08 20:00"
+
 @app.route("/")
 def home():
+    print("DEBUG: home page in Flask:")  # Print to console
+    return render_template("index.html")
+
+
+@app.route("/f1")
+def f1():
 	print("DEBUG: Race Date in Flask:", NEXT_RACE["date"])  # Print to console
-	return render_template("index.html", race=NEXT_RACE)
+	return render_template("f1.html", race=NEXT_RACE)
+
+@app.route("/for-the-best-girl-in-the-world")
+def app1():
+	print("DEBUG: loaded timer")
+	return render_template("for-my-girl.html", counter_start=counter_time)
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000, debug=True)
